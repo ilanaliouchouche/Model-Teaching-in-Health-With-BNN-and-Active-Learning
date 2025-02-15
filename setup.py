c1d6e12b557e8b2df090b230ce9e7811aee03c8b
Ilan Aliouchouche <ialiouch@margpu007.localdomain>
2025-02-15 15:19:32 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Model-Teaching-in-Health-With-BNN-and-Active-Learning',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    author='Ilan Aliouchouche',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'tqdm',
+        'tensorboard',
+        'seaborn',
+        'torch',
+        'medmnist',
+        'torchbnn'
+    ],
+    python_requires='>=3.11',
+)
